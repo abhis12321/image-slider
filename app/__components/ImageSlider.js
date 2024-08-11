b1634be41312cf6414f44610b60562250d9e0674
abhis12321 <abhisheksingh2359328@gmail.com>
2024-08-11 15:05:55 +0530

    onCLick event handler is added to handle next and previous images

--- app/__components/ImageSlider.js
+++ app/__components/ImageSlider.js
@@ -5,13 +5,11 @@ export default function ImageSlider({ imagesUrl }) {
     const [activeImageIndex, setActiveImageIndex] = useState(0);
     return (
         <div className="w-full h-[100vh] flex items-center justify-center gap-6">
-            <button className="">prev</button>
-            <div className='w-[800px]'>
-                {
-                    imagesUrl?.map((imageUrl, index) => <img src={imageUrl} alt="" className={`w-full duration-700 bg-red-500 ${activeImageIndex !== index && "hidden"}`} key={index} />)
-                }
-            </div>
-            <button className="">next</button>
+            <button className="" onClick={() => setActiveImageIndex(prev => (prev + imagesUrl.length - 1) % imagesUrl.length)}>prev</button>
+            {
+                imagesUrl?.map((imageUrl, index) => <img src={imageUrl} alt="" className={`w-[100%] duration-700 delay-700 bg-red-500 ${activeImageIndex !== index && "hidden"}`} key={index} />)
+            }
+            <button className="" onClick={() => setActiveImageIndex(prev => (prev + 1) % imagesUrl.length)}>next</button>
         </div>
     )
 }
