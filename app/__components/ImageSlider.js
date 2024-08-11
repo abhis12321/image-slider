4a639161c992f417a47b9c2fed3bf8d236b99665
abhis12321 <abhisheksingh2359328@gmail.com>
2024-08-11 15:00:13 +0530

    some stylings are added to ImageSlider component

--- app/__components/ImageSlider.js
+++ app/__components/ImageSlider.js
@@ -1,15 +1,17 @@
 "use client"
 import React, { useState } from 'react'
 
-export default function ImageSlider( {imagesUrl} ) {
-  const [activeImageIndex , setActiveImageIndex] = useState(0);
-  return (
-    <div className='w-[800px] h-[500px]'>
-      <button className="">prev</button>
-      {
-        imagesUrl?.map((imageUrl , index) => <img src={imageUrl} alt="" className={`w-full h-full duration-700 bg-red-500 ${activeImageIndex !== index && "hidden"}`} key={index}/>)
-      }
-      <button className="">next</button>
-    </div>
-  )
+export default function ImageSlider({ imagesUrl }) {
+    const [activeImageIndex, setActiveImageIndex] = useState(0);
+    return (
+        <div className="w-full h-[100vh] flex items-center justify-center gap-6">
+            <button className="">prev</button>
+            <div className='w-[800px]'>
+                {
+                    imagesUrl?.map((imageUrl, index) => <img src={imageUrl} alt="" className={`w-full duration-700 bg-red-500 ${activeImageIndex !== index && "hidden"}`} key={index} />)
+                }
+            </div>
+            <button className="">next</button>
+        </div>
+    )
 }
