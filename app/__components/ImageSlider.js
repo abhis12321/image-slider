803c2cfe6a1c3707150d9afdad303b6892916cac
abhis12321 <abhisheksingh2359328@gmail.com>
2024-08-11 14:51:08 +0530

    useState hook is used for handling activate single image at a time

--- app/__components/ImageSlider.js
+++ app/__components/ImageSlider.js
@@ -1,11 +1,13 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 export default function ImageSlider( {imagesUrl} ) {
+  const [activeImageIndex , setActiveImageIndex] = useState(0);
   return (
-    <div className='w-full h-[500px]'>
+    <div className='w-[800px] h-[500px]'>
       <button className="">prev</button>
       {
-        imagesUrl?.map((imageUrl , index) => <img src={imageUrl} alt="" className={`w-full h-full duration-700 bg-red-500`} key={index}/>)
+        imagesUrl?.map((imageUrl , index) => <img src={imageUrl} alt="" className={`w-full h-full duration-700 bg-red-500 ${activeImageIndex !== index && "hidden"}`} key={index}/>)
       }
       <button className="">next</button>
     </div>
